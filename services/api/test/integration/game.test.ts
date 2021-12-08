c30bd7e258ee05153f088dd3d8abea88b9f04fdf
tliekis <tautvydas.liekis@sony.com>
2021-12-08 15:17:32 +0200

    tests

--- services/api/test/integration/game.test.ts
+++ services/api/test/integration/game.test.ts
@@ -1,13 +1,25 @@
 import request from 'supertest';
 import app from '../../src/app';
+import { v4 as randomUUID } from 'uuid';
 
 describe('/api/game', function () {
-  describe('GET /test', function () {
+  describe(`GET /${randomUUID()}`, function () {
     it('should return status 200 and message', async function () {
       await request(app)
-        .get('/api/game/test')
+        .get(`/api/game/${randomUUID()}`)
         .expect('Content-Type', 'application/json; charset=utf-8')
         .expect(200, { test: true });
+    });
+  });
+});
+
+describe('/api/game', function () {
+  describe('GET /123-456', function () {
+    it('should return status 200 and message', async function () {
+      await request(app)
+        .get('/api/game/123-456')
+        .expect('Content-Type', 'application/json; charset=utf-8')
+        .expect(200, { test: false });
     });
   });
 });
