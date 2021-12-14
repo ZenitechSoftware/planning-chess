a3f18043b2d126a1964d52d73e9f0fac1dc17e37
tliekis <tautvydas.liekis@sony.com>
2021-12-14 20:04:34 +0200

    copy link

--- services/app/src/pages/game/Game.jsx
+++ services/app/src/pages/game/Game.jsx
@@ -2,11 +2,26 @@ import React from 'react';
 
 const Room = () => {
   const currentGameURL = window.location.href;
+  const gameURL =
+    window.location.host + '/game/671e2367-86c3-453a-9df8-3c0048145b64';
+
+  const copyGameURL = async (url) => {
+    try {
+      await navigator.clipboard.writeText(url);
+    } catch (err) {
+      new Error(err);
+    }
+  };
 
   return (
     <div>
       <h1 id="game">GAME</h1>
       <a id="game-url">{currentGameURL}</a>
+      <div>
+        <button id="copy-game-url-button" onClick={() => copyGameURL(gameURL)}>
+          Copy Link
+        </button>
+      </div>
     </div>
   );
 };
