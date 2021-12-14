586afdd9019f7fb574fcec43a22d91026cbcddc0
tliekis <tautvydas.liekis@sony.com>
2021-12-15 01:11:57 +0200

    test fixes

--- services/app/jest.config.js
+++ services/app/jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleNameMapper: {
+    '\\.(css|less)$': '<rootDir>/src/test/__mocks__/styleMock.js',
+  },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
 };
