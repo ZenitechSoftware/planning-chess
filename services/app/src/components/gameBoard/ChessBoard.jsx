5f9176b2369bf42119e3aeae816bc34f5e1d0cdd
Mykolas <>
2021-12-15 17:10:19 +0200

    Change proc code to func code

--- services/app/src/components/gameBoard/ChessBoard.jsx
+++ services/app/src/components/gameBoard/ChessBoard.jsx
@@ -1,59 +1,30 @@
 import React from 'react';
 import '../../static/style/chess-board.css';
 
-// eslint-disable-next-line react/prop-types
 const ChessBoard = ({ numberOfRows, numberOfCells }) => {
-  const rowNumber = numberOfRows;
-  const rowCellNumber = numberOfCells;
+  const board = [...Array(numberOfRows).keys()].map((_row, rowIndex) =>
+    [...Array(numberOfCells).keys()].map((_tile, tileIndex) => ({
+      isFilled:
+        (rowIndex % 2 && !(tileIndex % 2)) ||
+        (!(rowIndex % 2) && tileIndex % 2),
+    })),
+  );
 
-  const squares = [];
-  const color = ['white_tile', 'black_tile'];
-
-  let useColorIndx = 0;
-  let colorSwitch = false;
-  let cellNo = 0;
-
-  for (let row = 1; row <= rowNumber; row++) {
-    let cell = [];
-    for (let rowCell = 1; rowCell <= rowCellNumber; rowCell++) {
-      cellNo++;
-      if (colorSwitch) {
-        if (rowCell % 2 === 0) {
-          useColorIndx = 0;
-        } else {
-          useColorIndx = 1;
-        }
-      } else {
-        if (rowCell % 2 === 0) {
-          useColorIndx = 1;
-        } else {
-          useColorIndx = 0;
-        }
-      }
-
-      cell.push(
-        <td
-          id={'cell_' + cellNo.toString()}
-          title={color[useColorIndx]}
-          key={cellNo}
-        >
-          {cellNo}
-        </td>,
-      );
-    }
-    squares.push(
-      <tr id={'row_' + row.toString()} key={'rowNo_' + row}>
-        {cell}
-      </tr>,
-    );
-    if (!colorSwitch) {
-      colorSwitch = true;
-    } else {
-      colorSwitch = false;
-    }
-  }
-
-  return <table id="chess_board">{squares}</table>;
+  return (
+    <table id="chess-board">
+      {board.map((row, rowIndex) => (
+        <tr id={'row_' + rowIndex} key={rowIndex}>
+          {row.map((tile, tileIndex) => (
+            <td
+              id={'cell_' + tileIndex}
+              class={tile.isFilled ? 'black-tile' : 'white-tile'}
+              key={tileIndex}
+            />
+          ))}
+        </tr>
+      ))}
+    </table>
+  );
 };
 
 export default ChessBoard;
