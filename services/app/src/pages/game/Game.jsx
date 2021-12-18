2fc916ac8fd653cf62a6da3d57c89cb152875c59
tautvydasLiekis <62600250+tautvydasLiekis@users.noreply.github.com>
2021-12-18 23:07:23 +0200

    chess board (#23)

--- services/app/src/pages/game/Game.jsx
+++ services/app/src/pages/game/Game.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ChessBoard from '../../components/gameBoard/ChessBoard';
 
 const Room = () => {
   return (
     <div>
       <h1>GAME</h1>
+      <ChessBoard numberOfCells={6} numberOfRows={6} />
     </div>
   );
 };
