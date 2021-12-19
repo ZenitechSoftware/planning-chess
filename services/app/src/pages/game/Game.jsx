77a5a40c69860ee16eeda4b7d709673ff8893531
tautvydasLiekis <tautvydas.liekis@gmail.com>
2021-12-19 16:41:39 +0200

    format fixes

--- services/app/src/pages/game/Game.jsx
+++ services/app/src/pages/game/Game.jsx
@@ -6,7 +6,8 @@ const Room = () => {
 
   const copyGameURL = async () => {
     try {
-      const url = window.location.host + '/game/671e2367-86c3-453a-9df8-3c0048145b64';
+      const url =
+        window.location.host + '/game/671e2367-86c3-453a-9df8-3c0048145b64';
       await navigator.clipboard.writeText(url);
       alert(`Link Copied To: \n ${url}`);
     } catch (err) {
@@ -17,7 +18,9 @@ const Room = () => {
   return (
     <div>
       <h1>GAME</h1>
-      <span id="game-url"><a onClick={() => copyGameURL()}>{currentGameURL}</a></span>
+      <span id="game-url">
+        <a onClick={() => copyGameURL()}>{currentGameURL}</a>
+      </span>
       <ChessBoard numberOfCells={6} numberOfRows={6} />
     </div>
   );
