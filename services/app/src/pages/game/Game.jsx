1777b0c456ce27fd743809eb5c7526a3947cdd3f
tautvydasLiekis <tautvydas.liekis@gmail.com>
2021-12-19 16:37:27 +0200

    fixes

--- services/app/src/pages/game/Game.jsx
+++ services/app/src/pages/game/Game.jsx
@@ -3,12 +3,12 @@ import ChessBoard from '../../components/chessBoard/ChessBoard';
 
 const Room = () => {
   const currentGameURL = window.location.href;
-  const gameURL =
-    window.location.host + '/game/671e2367-86c3-453a-9df8-3c0048145b64';
 
-  const copyGameURL = async (url) => {
+  const copyGameURL = async () => {
     try {
+      const url = window.location.host + '/game/671e2367-86c3-453a-9df8-3c0048145b64';
       await navigator.clipboard.writeText(url);
+      alert(`Link Copied To: \n ${url}`);
     } catch (err) {
       new Error(err);
     }
@@ -17,7 +17,7 @@ const Room = () => {
   return (
     <div>
       <h1>GAME</h1>
-      <span id="game-url">{currentGameURL}</span>
+      <span id="game-url"><a onClick={() => copyGameURL()}>{currentGameURL}</a></span>
       <ChessBoard numberOfCells={6} numberOfRows={6} />
     </div>
   );
