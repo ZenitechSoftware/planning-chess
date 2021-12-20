b07558a4aee6be8be69ab234f9d06a4ae150c756
tautvydasLiekis <62600250+tautvydasLiekis@users.noreply.github.com>
2021-12-20 06:56:31 +0200

    display link in game view (#21)

--- services/app/src/pages/game/Game.jsx
+++ services/app/src/pages/game/Game.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import ChessBoard from '../../components/gameBoard/ChessBoard';
+import ChessBoard from '../../components/chessBoard/ChessBoard';
 
 const Room = () => {
+  const currentGameURL = window.location.href;
+
   return (
     <div>
       <h1>GAME</h1>
+      <span id="game-url">{currentGameURL}</span>
       <ChessBoard numberOfCells={6} numberOfRows={6} />
     </div>
   );
