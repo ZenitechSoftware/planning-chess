b61bdf87305c0750c01f607cabcd46f4479d1219
tautvydasLiekis <tautvydas.liekis@gmail.com>
2021-12-21 21:20:44 +0200

    format

--- services/app/src/components/chessBoard/ChessBoardPieces.jsx
+++ services/app/src/components/chessBoard/ChessBoardPieces.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 const ChessBoardPieces = () => {
-
   const pieces = ['pawn', 'knight', 'bishop', 'rook', 'king', 'queen'];
-    
+
   return (
     <div id="chess-game-pieces">
-      { pieces.map((figure) => (
-        <p className={figure+'-piece'} key={figure}>{figure}</p>
+      {pieces.map((figure) => (
+        <p className={figure + '-piece'} key={figure}>
+          {figure}
+        </p>
       ))}
-    </div>  
+    </div>
   );
 };
 
