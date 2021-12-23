6489e938c4b9d249c2097f2e57558114df9c2724
Mykolas <>
2021-12-23 15:36:54 +0200

    Enforce app test coverage

--- services/app/jest.config.js
+++ services/app/jest.config.js
@@ -6,4 +6,14 @@ module.exports = {
     '\\.(css|less)$': '<rootDir>/src/test/__mocks__/styleMock.js',
   },
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
