c2d2912ec1c70ec2f4716356cd6fcca03c8a134a
tautvydasLiekis <tautvydas.liekis@gmail.com>
2021-12-26 14:10:50 +0200

    test

--- services/app/src/test/DOM/Pages/game/game.test.jsx
+++ services/app/src/test/DOM/Pages/game/game.test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Game from '../../../../pages/game/Game';
 
 test('test Game page elements', () => {
   render(<Game />);
   expect(screen.getByText('GAME')).toBeInTheDocument();
   expect(screen.getByText('http://localhost/')).toBeInTheDocument();
+  fireEvent.click(screen.getByText('Copy link'));
+  const pieces = ['pawn', 'knight', 'bishop', 'rook', 'king', 'queen'];
+  pieces.forEach(p => {
+    expect(screen.getByText(p)).toBeInTheDocument();
+  });
 });
