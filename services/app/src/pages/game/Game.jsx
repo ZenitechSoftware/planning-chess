80a383ef95e921c6820d6898c4e8bc784d00592d
tautvydasLiekis <62600250+tautvydasLiekis@users.noreply.github.com>
2021-12-28 10:20:59 +0200

    display chess pieces (#26)

--- services/app/src/pages/game/Game.jsx
+++ services/app/src/pages/game/Game.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ChessBoard from '../../components/chessBoard/ChessBoard';
+import ChessBoardPieces from '../../components/chessBoard/ChessBoardPieces';
 
 function Room() {
   const [roomUrl] = useState(window.location.href);
@@ -12,6 +13,7 @@ function Room() {
         <button type="button">Copy link</button>
       </CopyToClipboard>
       <ChessBoard numberOfColumns={6} numberOfRows={6} />
+      <ChessBoardPieces />
     </div>
   );
 }
