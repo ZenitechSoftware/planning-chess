a3bf55af57123e6d448b6e7b68f2688ac9f0d112
tautvydasLiekis <tautvydas.liekis@gmail.com>
2022-01-11 20:38:14 +0200

    formatting

--- services/app/src/components/player/Player.jsx
+++ services/app/src/components/player/Player.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../../static/style/username.css';
 
-const Player = ({ name}) => (
+const Player = ({ name }) => (
   <div id="userName">
     <span>{name}</span>
   </div>
 );
 Player.defaultProps = {
-    name: ""
-}
+  name: '',
+};
 Player.propTypes = {
   name: PropTypes.string,
 };
