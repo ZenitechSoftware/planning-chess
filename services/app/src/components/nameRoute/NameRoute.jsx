de0fec2cb0f799f464ad7d0780d127838f02dbd7
tautvydasLiekis <tautvydas.liekis@gmail.com>
2022-01-31 22:21:32 +0200

    push

--- services/app/src/components/nameRoute/NameRoute.jsx
+++ services/app/src/components/nameRoute/NameRoute.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { useUserFromLocalStorage } from '../../hooks/useUserFromLocalStorage';
+import UserModal from "../modal/UserModal";
 
 const NameRoute = () => {
   const { authentication } = useUserFromLocalStorage();
-  return authentication && <Outlet />;
+  return authentication ? <Outlet /> : <UserModal />;
 };
 
 export default NameRoute;
--- services/app/src/hooks/useUserFromLocalStorage.js
+++ services/app/src/hooks/useUserFromLocalStorage.js
@@ -9,11 +9,6 @@ export const useUserFromLocalStorage = () => {
     if (userFromLocalStorage) {
       setUser(userFromLocalStorage);
       setAuth(true);
-    } else {
-      const person = prompt('Please enter your name');
-      window.localStorage.setItem('user', person);
-      setUser(userFromLocalStorage);
-      setAuth(true);
     }
   }, []);
   return { authentication: auth, username: user };
