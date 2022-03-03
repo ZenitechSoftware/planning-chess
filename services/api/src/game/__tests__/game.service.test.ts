70488d3a3a0d725d7cf3fbecf3b4fb9c662966db
Albertas <albertas.kruzintaitis@zenitech.co.uk>
2022-03-03 10:00:16 +0200

    tests

--- services/api/src/game/__tests__/game.service.test.ts
+++ services/api/src/game/__tests__/game.service.test.ts
@@ -2,7 +2,7 @@ import * as gameService from '../game.service';
 
 describe('game.service', () => {
   it('should return new game state', async () => {
-    const newState = gameService.figureMoved({});
+    const newState = gameService.figureMoved({ row: 1, tile: 1, figure: 'rock', player: 'test' });
     expect(newState).toMatchSnapshot(newState);
   });
   it('should return empty list when all players are done', async () => {
