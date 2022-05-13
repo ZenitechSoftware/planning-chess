3a7dcc74d6479c195ec43a8c2c84ec99556ffab5
Justinas Valatkevicius <justinas.valatkevicius@zenitech.org.uk>
2022-05-13 20:05:09 +0300

    fix cspell warnings

--- services/app/src/hooks/useChessBoardContext.js
+++
@@ -1,6 +0,0 @@
-import { ChessBoardContext } from '../contexts/ChessBoardContext';
-import { useContext } from 'react';
-
-export const useChessBoardContext = () => {
-    return useContext(ChessBoardContext);
-};
