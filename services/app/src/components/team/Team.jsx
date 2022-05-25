6d044d9c086c0ab8943cb37f332bee88fa3c3e86
Robert Orlov <orroru@gmail.com>
2022-05-25 14:01:30 +0300

    Add visual design to player list

--- services/app/src/components/team/Team.jsx
+++ services/app/src/components/team/Team.jsx
@@ -2,23 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TeamMember from './TeamMember';
 import userPropType from '../../prop-types/user';
+import Return from './teamComponents/Return.svg';
 import '../../static/style/team.css';
 
 function Team({ title, users, skipMove, children, removePlayer }) {
+
+  const playerCount = Array.isArray(users) && users.length > 0 ? `${users.length + 1} players` : '1 player';
+
   return (
     <div className="team-container">
-      <h2>{title}</h2>
-      <div className="team-list-item">{children}</div>
-      {users.map((user) => (
-        <TeamMember
-          key={user.id}
-          name={user.name}
-          id={user.id}
-          skipMove={skipMove}
-          status={user.status}
-          removePlayer={removePlayer}
-        />
-      ))}
+
+      <div className="team-heading">{title}</div>
+      <div className="team-list-count">
+        {playerCount}
+      </div>
+
+      <div className="team-list-items">
+        <div className="team-list-item">{children}</div>
+        {users.map((user) => (
+          <TeamMember
+            key={user.id}
+            name={user.name}
+            id={user.id}
+            skipMove={skipMove}
+            status={user.status}
+            removePlayer={removePlayer}
+          />
+        ))}
+      </div>
+      <div className="team-list-footer">
+        <button><img src={Return} /> Restart game</button>
+      </div>
     </div>
   );
 }
--- services/app/src/components/team/TeamMember.jsx
+++ services/app/src/components/team/TeamMember.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import playerStatuses from '../../constants/playerStatuses';
+import { getPlayerAvatarColor } from '../../helpers/getPlayerAvatarColor';
+import Skip from './teamComponents/Skip.svg';
+import Remove from './teamComponents/Remove.svg';
 import '../../static/style/team.css';
 
 function TeamMember({ name, id, skipMove, status, removePlayer }) {
+  const playerColor = getPlayerAvatarColor();
   return (
     <div className="team-list-item">
+      <div className="team-list-item-avatar" style={{
+        backgroundColor: `rgb(${playerColor.background.r}, ${playerColor.background.g}, ${playerColor.background.b})`
+      }}>
+          <div 
+            style={{
+              color: `rgb(${playerColor.text.r}, ${playerColor.text.g}, ${playerColor.text.b})`
+            }}
+          >{name[0].toUpperCase()}</div>
+      </div>
       {name}
-      <button
-        type="button"
-        disabled={status !== playerStatuses.ActionNotTaken}
-        onClick={() => skipMove(id)}
-      >
-        skip
-      </button>
-      <button type="button" onClick={() => removePlayer(id)}>
-        remove
-      </button>
+      <div className="team-list-item-actions">
+          <button onClick={() => skipMove(id)}><img src={Skip} /></button>
+          <button onClick={() => removePlayer(id)}><img src={Remove} /></button>
+        </div>
     </div>
   );
 }
