5898c1e0a7b0d7b2c6a86dc8d131dc565f688a35
Edvardas Markevicius <edvardas.markevicius@zenitech.co.uk>
2022-05-25 10:08:15 +0300

    Address code review

--- services/app/src/components/chessBoard/ChessBoard.jsx
+++ services/app/src/components/chessBoard/ChessBoard.jsx
@@ -13,26 +13,28 @@ const ChessBoard = () => {
         <span className="rotated-inner axis-title">Annotation of Work</span>
       </div>
       <table id="chess-board">
-        {board.map((row, rowIndex) => (
-          <tr key={rowIndex}>
-            {row.map((tile, columnIndex) => (
-              <Tile
-                key={columnIndex}
-                tile={tile}
-                onClick={placeItemOnBoard}
-                row={rowIndex}
-                column={columnIndex}
-                boardLength={board.length}
-              />
-            ))}
+        <tbody>
+          {board.map((row, rowIndex) => (
+            <tr key={rowIndex}>
+              {row.map((tile, columnIndex) => (
+                <Tile
+                  key={columnIndex}
+                  tile={tile}
+                  onClick={placeItemOnBoard}
+                  row={rowIndex}
+                  column={columnIndex}
+                  boardLength={board.length}
+                />
+              ))}
+            </tr>
+          ))}
+          <tr>
+            <td />
+            <td colSpan={board.length - 1}>
+              <span className="axis-title">Uncertainty</span>
+            </td>
           </tr>
-        ))}
-        <tr>
-          <td />
-          <td colSpan={board.length - 1}>
-            <span className="axis-title">Uncertainty</span>
-          </td>
-        </tr>
+        </tbody>
       </table>
     </div>
   );
