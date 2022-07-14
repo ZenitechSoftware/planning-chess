882fd5e627165027a1e2d8612490f9a9e31b6fdf
arvydas.urbonas <urbonas.a@cambridgeassessment.org.uk>
2022-07-14 11:41:24 +0300

    updated test

--- services/app/src/test/DOM/Pages/game/game.test.jsx
+++ services/app/src/test/DOM/Pages/game/game.test.jsx
@@ -4,34 +4,52 @@ import Game from '../../../../pages/game/Game';
 import Player from '../../../../components/player/Player';
 import ChessGameProvider from '../../../../contexts/ChessBoardContext';
 
-test('test Game page elements', () => {
-  localStorage.setItem('user', 'TestUser')
-  render(
-    <ChessGameProvider>
-      <Game />
-    </ChessGameProvider>,
-  );
-  expect(screen.getByText('Annotation of Work')).toBeInTheDocument();
-  expect(screen.getByText('Uncertainty')).toBeInTheDocument();
-  expect(screen.getByText('Complexity | Select and place the figure on the board')).toBeInTheDocument();
-  fireEvent.click(screen.getByAltText('copy link'));
+describe('Game', () => {
+  test('test Game page elements', () => {
+    localStorage.setItem('user', 'TestUser')
+    render(
+      <ChessGameProvider>
+        <Game />
+      </ChessGameProvider>,
+    );
+    expect(screen.getByText('Annotation of Work')).toBeInTheDocument();
+    expect(screen.getByText('Uncertainty')).toBeInTheDocument();
+    expect(screen.getByText('Complexity | Select and place the figure on the board')).toBeInTheDocument();
+    fireEvent.click(screen.getByAltText('copy link'));
+  });
+
+  test('test Player page elements', () => {
+    render(
+      <Player
+        player={{
+          name: 'username',
+          id: '1',
+          status: 'ActionNotTaken',
+          color: { background: {
+              r: 1,
+              g: 1,
+              b: 1,
+            },
+            text: {
+              r: 1,
+              g: 1,
+              b: 1,
+            }
+          }
+      }}
+        skipMove={() => {}}
+        score={0}
+      />,
+    );
+    expect(screen.getByText('U', {
+      selector: ".team-list-item-avatar-text"
+    })).toBeInTheDocument();
+    expect(screen.getByText('username', {
+      selector: ".team-list-item-name"
+    })).toBeInTheDocument();
+    expect(screen.getByText('(you)', {
+      selector: ".team-list-item-name > span"
+    })).toBeInTheDocument();
+  });
 });
 
-test('test Player page elements', () => {
-  render(
-    <Player
-      player={{ name: 'username', id: '1', status: 'ActionNotTaken' }}
-      skipMove={() => {}}
-      score={0}
-    />,
-  );
-  expect(screen.getByText('U', {
-    selector: ".team-list-item-avatar-text"
-  })).toBeInTheDocument();
-  expect(screen.getByText('username', {
-    selector: ".team-list-item-name"
-  })).toBeInTheDocument();
-  expect(screen.getByText('(you)', {
-    selector: ".team-list-item-name > span"
-  })).toBeInTheDocument();
-});
