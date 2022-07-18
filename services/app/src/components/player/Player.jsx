338071164a8935ae223e7f7743e45bb5fbe67380
arvydas.urbonas <urbonas.a@cambridgeassessment.org.uk>
2022-07-18 11:01:08 +0300

    formatting

--- services/app/src/components/player/Player.jsx
+++ services/app/src/components/player/Player.jsx
@@ -4,42 +4,41 @@ import playerStatuses from '../../constants/playerStatuses';
 import playerPropType from '../../prop-types/player';
 import Skip from '../team/teamComponents/Skip.svg';
 
-const Player = ({ player, skipMove }) => {
-  return (
-    player && (
-    <>
-        <div
-          className="team-list-item-avatar"
-          style={{
-            backgroundColor: `rgb(${player.color.background.r}, ${player.color.background.g}, ${player.color.background.b})`,
-          }}
-        >
+const Player = ({ player, skipMove }) => (
+        player && (
+        <>
           <div
-            className="team-list-item-avatar-text"
+            className="team-list-item-avatar"
             style={{
-              color: `rgb(${player.color.text.r}, ${player.color.text.g}, ${player.color.text.b})`,
-            }}
-        >
-          {player.name[0].toUpperCase()}
-        </div>
-      </div>
-      <div className="team-list-item-name">
-        {player.name}
-        {' '}
-        <span>(you)</span>
-      </div>
-      <div className="team-list-item-actions">
-        <button
-          type="button"
-          onClick={() => skipMove(player.id)}
-          disabled={player.status !== playerStatuses.ActionNotTaken}
-        >
-          <img alt="" src={Skip} />
-        </button>
-      </div>
-    </>
-    )
-  );
+                        backgroundColor: `rgb(${player.color.background.r}, ${player.color.background.g}, ${player.color.background.b})`,
+                    }}
+          >
+            <div
+              className="team-list-item-avatar-text"
+              style={{
+                            color: `rgb(${player.color.text.r}, ${player.color.text.g}, ${player.color.text.b})`,
+                        }}
+            >
+              {player.name[0].toUpperCase()}
+            </div>
+          </div>
+          <div className="team-list-item-name">
+            {player.name}
+            {' '}
+            <span>(you)</span>
+          </div>
+          <div className="team-list-item-actions">
+            <button
+              type="button"
+              onClick={() => skipMove(player.id)}
+              disabled={player.status !== playerStatuses.ActionNotTaken}
+            >
+              <img alt="" src={Skip} />
+            </button>
+          </div>
+        </>
+        )
+    );
 Player.defaultProps = {
   player: null,
 };
