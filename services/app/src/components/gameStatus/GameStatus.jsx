5e577859371a311a05643ce63d5c2b63c4aa0aa1
arvydas.urbonas <urbonas.a@cambridgeassessment.org.uk>
2022-07-18 11:14:26 +0300

    game status fixed

--- services/app/src/components/gameStatus/GameStatus.jsx
+++ services/app/src/components/gameStatus/GameStatus.jsx
@@ -1,38 +1,27 @@
-import React, {useEffect, useContext, useState} from 'react';
+import React, { useContext } from 'react';
 import './gameStatus.css';
 import classnames from 'classnames';
 import ClockIcon from './statusComponents/clock.svg';
 import CompletedIcon from './statusComponents/completedIcon.svg';
-import {useWebSockets} from "../../utils/useWebSockets";
 import {ChessBoardContext} from "../../contexts/ChessBoardContext";
-import {buildPlayerConnectedEventMessage} from "../../api/playerApi";
 
 function GameStatus() {
-  const [gameFinished, setGameFinished] = useState(false);
   const SpScore = 0;
-  const {isAllTurnsMade} = useContext(ChessBoardContext);
-  console.log(isAllTurnsMade);
+  const { isAllTurnsMade } = useContext(ChessBoardContext);
 
-  console.log()
-  useEffect(() => {
-    if (isAllTurnsMade) {
-      setGameFinished(true);
-    }
-  }, [isAllTurnsMade]);
-
-  const gameStatusText = gameFinished ? `Game complete - ${SpScore}SP` : "Game in progress...";
-  const iconScr = gameFinished ? CompletedIcon : ClockIcon;
+  const gameStatusText = isAllTurnsMade ? `Game complete - ${SpScore}SP` : "Game in progress...";
+  const iconScr = isAllTurnsMade ? CompletedIcon : ClockIcon;
 
   return (
-    <div3
+    <div
       className={classnames("game-status-field", {
-        "game-status-in-progress": !gameFinished,
-        "game-status-completed": gameFinished,
+        "game-status-in-progress": !isAllTurnsMade,
+        "game-status-completed": isAllTurnsMade,
       })}
     >
       <img src={iconScr} alt="game status icon" className="game-status-icon" />
       <p className="status-text">{gameStatusText}</p>
-    </div3>
+    </div>
   );
 }
 
