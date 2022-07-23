d4866320844f70d7caf65e075214f564b53e3c95
Mykolas <>
2022-07-23 13:35:03 +0300

    Ping clients in order not to close WS connection

--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -6,6 +6,7 @@ import * as path from 'path';
 import app from './app';
 import { WebSocketServer } from 'ws';
 import * as playersService from './messaging/players.service';
+// import logger from './logger';
 
 app.use(express.static(path.join(__dirname, '/../../../app/dist')));
 app.get('*', (_req, res) => {
@@ -18,9 +19,20 @@ const wss = new WebSocketServer({
   noServer: true,
 });
 
+const interval = setInterval(() => {
+  wss.clients.forEach((ws) => {
+    ws.ping();
+  });
+}, 10000);
+
+wss.on('close', () => {
+  clearInterval(interval);
+});
+
 server.on('upgrade', (req, socket, head) => {
   wss.handleUpgrade(req, socket, head, (ws, request) => {
     wss.emit('connection', ws, request);
+    // ws.on('pong', () => logger.info(`Pong  ${ws}`));
     ws.on('message', (data) => {
       playersService.newMessageReceived(ws, JSON.parse(data.toString()));
       ws.on('close', () => {
