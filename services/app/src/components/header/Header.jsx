fbbc88573e2f7c38c3d841482ddfaf25eec7679a
AndzejIlj <55466198+AndzejIlj@users.noreply.github.com>
2022-08-21 22:09:01 +0300

    copy-icon-ripple-effect (#102)
    * copy-icon-ripple-effect
    
    * Small Changes As Requested
    
    * Changed from div to button
    
    * Reusable Ripple Button
    
    * Required changes done

--- services/app/src/components/header/Header.jsx
+++ services/app/src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { CopyToClipboard } from 'react-copy-to-clipboard';
+import RippleButton from '../rippleButton/RippleButton';
 import Logo from "./headerComponents/PlanningChessLogo.svg";
 import Separator from "./headerComponents/SolidSeparator.svg";
 import CopyLink from "./headerComponents/CopyLink.svg";
@@ -12,15 +12,23 @@ import '../../static/style/header.css';
 const Header = ({ username, roomUrl }) => { 
   const avatarLetter = username[0];
 
+  const copyUrl = () => {
+    navigator.clipboard.writeText(roomUrl);
+  }
+
   return (
     <div className="align-c" id="header">
       <div className="f-row align-c gap-m">
         <img src={Logo} alt="logo" />
         <img src={Separator} alt="separator" />
         {/* <span>RoomNameExample </span> */}
-        <CopyToClipboard text={roomUrl}>
-          <img src={CopyLink} alt="copy link" style={{ cursor: "pointer" }} />
-        </CopyToClipboard>
+
+        <div className="copy-section">
+          <RippleButton onClick={copyUrl}>
+            <img src={CopyLink} alt="copy link" className="copy-icon" />
+          </RippleButton>
+        </div>
+        
         {/* <img src={Settings} alt="nav-item" /> */}
         {/* <img src={Info} alt="nav-item" /> */}
       </div>
---
+++ services/app/src/components/rippleButton/RippleButton.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from "prop-types";
+import './styles.css';
+
+const RippleButton = ({ onClick, children }) => {
+    const [shouldRipple, setShouldRipple] = useState(false);
+    const [clickCoords, setClickCoords] = useState({ x: -1, y: -1 })
+
+    useEffect(() => {
+        if((clickCoords.x !== -1) && (clickCoords.y !== -1)) {
+            setShouldRipple(true);
+            setTimeout(() => setShouldRipple(false), 1000);
+        }
+    }, [clickCoords]);
+
+    useEffect(() => {
+        if(!shouldRipple) setClickCoords({ x: -1, y: -1 })
+    }, [shouldRipple]);
+
+    const handleClick = (e) => {
+        setClickCoords({
+            x: e.clientX - e.target.offsetLeft,
+            y: e.clientY - e.target.offsetTop,
+        })
+
+        onClick();
+    }
+
+
+    return (
+      <button type="button" className="copy-btn-container" onClick={(e) => handleClick(e)}>
+        {shouldRipple && <span className='ripple' style={{ left: `${clickCoords.x}px`, top: `${clickCoords.y}px` }} />}
+        {children}
+      </button>
+    )
+};
+
+RippleButton.propTypes = {
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.oneOfType([
+        PropTypes.string,
+        PropTypes.element,
+    ]).isRequired
+}
+
+export default RippleButton;
