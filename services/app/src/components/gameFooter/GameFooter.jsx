e33505e39c7dc6c43bc13cbe78b70936fe93fa07
Justas Bartka <>
2022-11-24 16:23:32 -0500

    Utilizing Material UI tooltip element

--- services/app/src/components/gameFooter/GameFooter.jsx
+++ services/app/src/components/gameFooter/GameFooter.jsx
@@ -3,6 +3,26 @@ import PropTypes from 'prop-types';
 import { ChessBoardContext } from '../../contexts/ChessBoardContext';
 import ChessBoardPieces from './ChessBoardPieces';
 import './chess-pieces.css';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Tooltip, { tooltipClasses } from '@mui/material/Tooltip';
+import Typography from '@mui/material/Typography';
+
+const ExplanatoryTooltip = styled(({ className, ...props }) => (
+  <Tooltip {...props} classes={{ popper: className }} title="Skips your move. The game continues" placement='right-end' arrow={true}/>
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: 'black',
+    color: 'white',
+    boxShadow: theme.shadows[1],
+    fontSize: 15,
+    height: 20
+  },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: 'black',
+  },
+}));
+
 
 function GameFooter({ skipCurrentPlayerMove }) {
   const { lastTurn, finishMove, finished, isAllTurnsMade } = useContext(ChessBoardContext);
@@ -18,12 +38,12 @@ function GameFooter({ skipCurrentPlayerMove }) {
         <button type="button" className="finish-btn" disabled={!lastTurn || isAllTurnsMade || (finished && !isAllTurnsMade)} onClick={finishMove}>
           Finish Move
         </button>
-        <div className='hoverText'>
-          <button type="button" className="skip-btn" disabled={finished} onClick={() => skipCurrentPlayerMove()}>
-            Skip Move
-          </button>
-          <div className="explText">Skips your move. The game continues</div>
-        </div>
+          <ExplanatoryTooltip>
+            <button type="button" className="skip-btn" disabled={finished} onClick={() => skipCurrentPlayerMove()}>
+              Skip Move
+            </button>
+          </ExplanatoryTooltip>
+          {/* <div className="expl-text">Skips your move. The game continues</div> */}
       </div>
     </div>
   );
