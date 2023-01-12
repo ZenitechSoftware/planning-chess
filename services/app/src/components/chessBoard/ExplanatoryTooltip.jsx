b5391f54fdd67faa93627dff48cda5ce61f5bc91
Justas Bartka <>
2023-01-12 18:36:52 -0500

    build error fixes

--- services/app/src/components/chessBoard/ExplanatoryTooltip.jsx
+++ services/app/src/components/chessBoard/ExplanatoryTooltip.jsx
@@ -1,27 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Tooltip } from 'antd';
 
-
-const ExplanatoryTooltip = ({
-    title,
-    children,
-    placement
-}) => {
-  return (
-    <Tooltip
-      title={title}
-      placement={placement}
-      overlayInnerStyle={{fontSize: 15}}
-    >
-        {children}
-    </Tooltip>
-  );
-};
+const ExplanatoryTooltip = ({ title, children, placement }) => (
+  <Tooltip
+    title={title}
+    placement={placement}
+    overlayInnerStyle={{ fontSize: 15 }}
+  >
+    {children}
+  </Tooltip>
+);
 
 ExplanatoryTooltip.propTypes = {
   title: PropTypes.string.isRequired,
   placement: PropTypes.string.isRequired,
+  children: PropTypes.element.isRequired,
 };
 
 export default ExplanatoryTooltip;
