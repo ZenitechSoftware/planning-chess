6d7bb90f8219efd365b7cd4eae17302c2b2ad095
Andrius Iljinych <andrius.iljinych@zenitech.co.uk>
2023-01-19 17:44:56 +0200

    PLAN-119 introduced classnames to Layout component

--- services/app/src/components/pageLayout/Layout.jsx
+++ services/app/src/components/pageLayout/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './layout.css';
 import Header from '../header/Header';
 import Footer from '../pageFooter/Footer';
 
 const Layout = ({ classnames, children }) => (
-  <div className={`page-layout f-column-between ${classnames}`}>
+  <div className={classNames('page-layout f-column-between', classnames)}>
     <Header />
     {children}
     <Footer />
