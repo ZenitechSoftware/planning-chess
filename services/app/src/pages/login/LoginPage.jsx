a6f7a5fb3e0e699ff0d9fec988d91fed6b328136
AndzejIlj <55466198+AndzejIlj@users.noreply.github.com>
2023-01-20 16:48:53 +0200

    PLAN-119-refactor-fe-layout-component (#197)
    * PLAN-119 introduced layout components and css vars
    
    * PLAN-119 required changes done
    
    * PLAN-119 introduced classnames to Layout component
    
    * PLAN-119 changed classname props
    
    * PLAN-199 fixed cspell json file
    
    Co-authored-by: Andrius Iljinych <andrius.iljinych@zenitech.co.uk>

--- services/app/src/pages/login/LoginPage.jsx
+++ services/app/src/pages/login/LoginPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Navigate } from 'react-router';
-import '../../static/style/loginScreen.css';
-import Header from '../../components/header/Header';
 import LoginForm from '../../components/loginPage/LoginForm';
-import Footer from '../../components/pageFooter/Footer';
 import { ROUTES } from '../routes';
 import { useUserContext } from '../../contexts/UserContext'; 
+import Layout from '../../components/pageLayout/Layout';
 
 const LoginPage = () => {
   const userContext = useUserContext();
@@ -13,11 +11,9 @@ const LoginPage = () => {
   return userContext.username ? (
     <Navigate to={ROUTES.roleSelection} replace />
   ) : (
-    <div className="login-screen f-column-between">
-      <Header />
+    <Layout className='page-layout-dark'>
       <LoginForm />
-      <Footer />
-    </div>
+    </Layout>
   )
 }
 
