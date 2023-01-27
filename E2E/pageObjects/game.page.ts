803b7d39b87e3b41200fbd52f3d6391aad5262ed
KristinaKrev <kristina.krevneviciute@zenitech.co.uk>
2023-01-27 16:41:49 +0200

    PLAN-114 changed locators and added to export

--- E2E/pageObjects/game.page.ts
+++ E2E/pageObjects/game.page.ts
@@ -1,43 +1,36 @@
 const { I } = inject();
 
 const locator = {
-      firstUserInList: '//div[contains(@data-testid, "0")]//div[@class="team-list-item-name"]',
-      secondUserInList: '//div[contains(@data-testid, "1")]//div[@class="team-list-item-name"]',
+  playersList: {
+    firstUserInList: '//div[contains(@data-testid, "0")]/div[@class="team-list-item-name"]',
+    secondUserInList: '//div[contains(@data-testid, "1")]/div[@class="team-list-item-name"]',
+    spectatorIcon: '//div[@class="team-list-item-avatar spectator-avatar f-center"]//img[@alt="spectator icon"]',
+  },
+  text: {
+    username: '#username',
+  },
+  chessBoard: {
+    board: '#chess-board',
+  },
+  chessPieces: {
+    container: '$chess-pieces-container',
+  },
+  buttons: {
+    copyLink: '//button/p[contains(text(), "Copy Link")]',
+    restartGame: '//button/span[contains(text(), "Restart game")]',
+  },
+};
 
-      username: '#username',
 
-      spectatorIcon: '//div[@class="team-list-item-avatar spectator-avatar f-center"]//img[@alt="spectator icon"]', 
+export = {
+  locator: locator,
 
-      chessBoard: '#chess-board',
-      chessPiecesContainer: '$chess-pieces-container',
+  waitingForMorePlayersStatus: () => {
+    I.see('Waiting for more players');
+    I.seeElement(locator.chessBoard.board);
+    I.dontSeeElement(locator.chessPieces.container);
+    I.seeElement(locator.buttons.copyLink);
+    I.seeElement(locator.buttons.restartGame);
+  }
 
-      buttons: {
-        copyLink: '//button[@class="copy-btn border-r-4 padding-sm gap-s align-c"]//p["Copy Link"]',
-        restartGame: '//button[@class="ant-btn css-sk7ap8 ant-btn-ghost border-r-4 padding-y-s weight-500 padding-x-m gap-s f-center margin-auto ghost-btn"]/span["Restart game"]',
-      }
-  };
-
-
-  export = {
-    checkFirsNameInList: (username: string) => {
-      I.see(username, locator.firstUserInList);
-    },
-    checkSecondNameInList: (username: string) => {
-      I.see(username, locator.secondUserInList);
-    },
-    spectatorIcon: () => {
-      I.waitForVisible(locator.spectatorIcon, 5);
-    },
-    checkUsername: (username: string) => {
-      I.seeTextEquals(username, locator.username)
-    },
-    waitingForMorePlayersStatus: () => {
-      I.see('Waiting for more players');
-      I.seeElement(locator.chessBoard);
-      I.dontSeeElement(locator.chessPiecesContainer);
-      I.seeElement(locator.buttons.copyLink);
-      I.seeElement(locator.buttons.restartGame);
-
-    }
-  
-  };
+};
