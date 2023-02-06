94512a80404ef3a120e259d81f4cd03bcf7574f5
Ruta Boguzaite <ruta.boguzaite@zenitech.co.uk>
2023-02-06 16:45:12 +0200

    changing let to const

--- E2E/assertions/copyLinkAssert.ts
+++ E2E/assertions/copyLinkAssert.ts
@@ -1,16 +1,15 @@
 import assert = require("assert");
-import { displayPartsToString } from "typescript";
 import gamePage = require("../pageObjects/game.page");
 const { I } = inject();
 
 export = {
     async checkIfTheLinkWasCopied () {
-      let currentUrl = await I.grabCurrentUrl();
+      const currentUrl = await I.grabCurrentUrl();
       I.waitForElement(gamePage.locator.buttons.copyLink);
       I.click(gamePage.locator.buttons.copyLink); 
       I.usePlaywrightTo('check clipboard', async ({ page, browserContext }) => {
         await browserContext.grantPermissions(['clipboard-read']);
-        let clipboardText = await page.evaluate("navigator.clipboard.readText()");
+        const clipboardText = await page.evaluate("navigator.clipboard.readText()");
         assert.strictEqual(currentUrl, clipboardText);  
       });
   },
