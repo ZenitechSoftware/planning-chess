99000fe90f42b0b7df5e532806a176b5b727aa77
Andrius Iljinych <andrius.iljinych@zenitech.co.uk>
2023-02-07 16:10:17 +0200

    PLAN-62 remove unnecessary margin

--- services/app/src/components/gameLayout/GameLayout.jsx
+++ services/app/src/components/gameLayout/GameLayout.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import './gameLayout.css';
 
 const GameLayout = ({ children }) => (
-  <div className='game-view margin-t-m margin-b-s margin-auto gap-sm'>
+  <div className='game-view margin-t-m margin-b-s gap-sm'>
     {children}
   </div>
 )
