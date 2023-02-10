2c8951923910f4b688fd7b75432a78769a096d78
KristinaKrev <119603613+KristinaKrev@users.noreply.github.com>
2023-02-10 10:32:35 +0200

    PLAN-122 voting functionality tests (#208)
    * PLAN-122 separate login and role selection pages
    
    * PLAN-122 create voting functionality tests
    
    * PLAN-122 resolve comments
    
    ---------
    
    Co-authored-by: Justinas Valatkevicius <justinas.valatkevicius@zenitech.org.uk>

--- E2E/pageObjects/game.page.ts
+++ E2E/pageObjects/game.page.ts
@@ -2,9 +2,10 @@ const { I } = inject();
 
 const locator = {
   playersList: {
-    firstUserInList: '//div[contains(@data-testid, "0")]/div[@class="team-list-item-name"]',
-    secondUserInList: '//div[contains(@data-testid, "1")]/div[@class="team-list-item-name"]',
-    spectatorIcon: '//div[@class="team-list-item-avatar spectator-avatar f-center"]//img[@alt="spectator icon"]',
+    firstUserInList: '//*[contains(@data-testid, "0")]/div[@class="team-list-item-name"]',
+    secondUserInList: '//*[contains(@data-testid, "1")]/div[@class="team-list-item-name"]',
+    spectatorIcon: '//*[@class="team-list-item-avatar spectator-avatar f-center"]//img[@alt="spectator icon"]',
+    playerDoneIcon: (username: string) => `//*[contains(@data-testid, '${username}')]/img[@alt='player done icon']`,
   },
   text: {
     username: '#username',
@@ -12,16 +13,21 @@ const locator = {
   },
   chessBoard: {
     board: '#chess-board',
+    chessTile: (tile: string) => `$chess-tile-${tile}`,
+    figureOnBoard: (tile: string, figure: string) => `//*[@data-testid='chess-tile-${tile}']//img[@alt='${figure}']`,
+    avatarOnBoard: (tile: string) => `//*[@data-testid='chess-tile-${tile}']/div[@class='bubble-container']//span[@class='name']`,
+    pointsOnBoard: (tile: string, value: string) => `//*[@data-testid='chess-tile-${tile}']//span[@class='figure-text'][contains(text(), '${value}')]`,
   },
   chessPieces: {
     container: '$chess-pieces-container',
+    figure: (figure: string) => `$${figure}-piece-btn`,
+    figureHighlighted: (figure: string) => `//button[@data-testid="${figure}-piece-btn"][contains(@class, "selected")]`,
   },
   buttons: {
     copyLink: locate('//*[text() = "Copy Link"]').at(1),
     restartGame: '//button/span[contains(text(), "Restart game")]',
   },
 };
-
 
 export = {
   locator: locator,
@@ -32,6 +38,22 @@ export = {
     I.dontSeeElement(locator.chessPieces.container);
     I.seeElement(locator.buttons.copyLink);
     I.seeElement(locator.buttons.restartGame);
+  },
+
+  vote:(figure: string, tile: string) => {
+    I.click(locator.chessPieces.figure(figure));
+    I.waitForVisible(locator.chessPieces.figureHighlighted(figure));
+    I.click(locator.chessBoard.chessTile(tile));
+  },
+  voteIsVisible:(figure: string, tile: string, value: string) => {
+    I.seeElement(locator.chessBoard.figureOnBoard(tile, figure));
+    I.seeElement(locator.chessBoard.avatarOnBoard(tile));
+    I.seeElement(locator.chessBoard.pointsOnBoard(tile, value));
+  },
+  voteIsNotVisible:(figure: string, tile: string, value: string) => {
+    I.dontSeeElement(locator.chessBoard.figureOnBoard(tile, figure));
+    I.dontSeeElement(locator.chessBoard.avatarOnBoard(tile));
+    I.dontSeeElement(locator.chessBoard.pointsOnBoard(tile, value));
   },
 
   checkCopyLinkButton: () => {
--- E2E/tests/copyLink.test.ts
+++ E2E/tests/copyLink.test.ts
@@ -9,17 +9,17 @@ Before(() => {
 });
 
 Scenario('Player can copy a game link',() => {
-  login.voterLogin(username.user1);
+  login.firstVoterLogin(username.user1);
   game.checkCopyLinkButton();
 });
 
 Scenario('Spectator can copy a game link',() => {
-    login.spectatorLogin(username.user2);
+    login.firstSpectatorLogin(username.user2);
     game.checkCopyLinkButton();
 });
 
 Scenario('Check if the link in the clipboard was copied correctly', async () => {
-  login.voterLogin(username.user1);
+  login.firstVoterLogin(username.user1);
   const currentUrl = await I.grabCurrentUrl();
   I.waitForElement(game.locator.buttons.copyLink);
   I.click(game.locator.buttons.copyLink); 
