1c20045d9fd84f60a5189f69c811471c28a8f559
KristinaKrev <119603613+KristinaKrev@users.noreply.github.com>
2023-02-24 09:07:53 +0200

    PLAN-113 spectator view automated tests (#219)
    * PLAN-113 spectator view tests
    
    * PLAN-113 renamed locator

--- E2E/pageObjects/game.page.ts
+++ E2E/pageObjects/game.page.ts
@@ -6,6 +6,7 @@ const locator = {
     playersList: '//div[contains(@data-testid, "list")]/div[@class="team-list-item-name"]',
     firstUserInList: '//*[contains(@data-testid, "0")]/div[@class="team-list-item-name"]',
     secondUserInList: '//*[contains(@data-testid, "1")]/div[@class="team-list-item-name"]',
+    playerUsernameByIndex: (rowNumberInList: number) => locate('//*[@class="team-list-item-name"]').at(rowNumberInList),
     spectatorIcon: '//*[@class="team-list-item-avatar spectator-avatar f-center"]//img[@alt="spectator icon"]',
     playerDoneIcon: (username: string) => `//*[contains(@data-testid, '${username}')]/img[@alt='player done icon']`,
     playerSkippedIcon: (username: string) => `//*[contains(@data-testid, '${username}')]/img[@alt='player skipped icon']`,
@@ -24,7 +25,7 @@ const locator = {
     pointsOnBoard: (tile: string, value: string) => `//*[@data-testid='chess-tile-${tile}']//span[@class='figure-text'][contains(text(), '${value}')]`,
   },
   chessPieces: {
-    container: '$chess-pieces-container',
+    container: '#chess-pieces-container',
     chessPiece: (chessPiece: ChessPiece) => `$${chessPiece}-piece-btn`,
     chessPieceHighlighted: (chessPiece: ChessPiece) => `//button[@data-testid="${chessPiece}-piece-btn"][contains(@class, "selected")]`,
   },
@@ -48,7 +49,19 @@ export = {
     I.seeElement(locator.buttons.copyLink);
     I.seeElement(locator.buttons.restartGame);
   },
-
+  gameInProgressStatus: () => {
+    I.see('Game in progress');
+    I.seeElement(locator.chessBoard.board);
+    I.seeElement(locator.chessPieces.container);
+    I.seeElement(locator.buttons.copyLink);
+    I.seeElement(locator.buttons.restartGame);
+  },
+  spectatorGameInProgressView: (username:string ) => {
+    I.seeTextEquals(`${username} (you)`, game.locator.playersList.playerUsernameByIndex(3));
+    I.seeElement(game.locator.playersList.spectatorIcon);
+    I.seeTextEquals(`${username}`, game.locator.text.username);
+    game.gameInProgressStatus();
+  },
   vote:(chessPiece: ChessPiece, tile: string) => {
     I.click(locator.chessPieces.chessPiece(chessPiece));
     I.waitForVisible(locator.chessPieces.chessPieceHighlighted(chessPiece));
---
+++ E2E/tests/spectatorView.test.ts
@@ -0,0 +1,137 @@
+Feature('spectator view');
+import username = require("../test_data/usernames");
+import ChessTile = require("../test_data/chessTiles");
+import { ChessPiece } from "../test_data/chessPieces";
+import { ChessPieceValue } from "../test_data/chessPieces";
+
+const { I, login, game } = inject();
+
+Scenario('Spectator can’t place selected figure on board', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user3);
+        I.click(game.locator.chessPieces.chessPiece(ChessPiece.rook));
+        I.dontSeeElement(game.locator.chessPieces.figureHighlighted(ChessPiece.rook));
+        I.click(game.locator.chessBoard.chessTile(ChessTile.e4));
+        game.voteIsNotVisible(ChessPiece.rook, ChessTile.e2, ChessPieceValue.rook)
+    });
+});
+
+Scenario('Spectator can’t skip move', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user3);
+        I.click(game.locator.buttons.skip);
+        I.dontSeeElement(game.locator.buttons.skipButtonHighlighted);
+        I.dontSeeElement(game.locator.playersList.playerSkippedIcon(username.user3));
+    });
+});
+
+Scenario('Spectator skips another player', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user3);
+        I.click(game.locator.buttons.skipOtherPlayer(username.user1))
+        I.waitForElement(game.locator.playersList.playerSkippedIcon(username.user1));
+    });
+    I.waitForElement(game.locator.playersList.playerSkippedIcon(username.user1));
+});
+
+Scenario('Spectator doesn’t have to vote for game to be finished', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user3);
+    });
+    session(username.user2, () => {
+        game.voteAndCheckThatVoteIsVisible(ChessPiece.pawn, ChessTile.a1, ChessPieceValue.pawn);
+    })
+    game.skipMove();
+    I.waitForText('Game complete');
+    game.voteIsVisible(ChessPiece.pawn, ChessTile.a1, ChessPieceValue.pawn);
+    session(username.user3, () => {
+        game.voteIsVisible(ChessPiece.pawn, ChessTile.a1, ChessPieceValue.pawn);
+    });
+});
+
+Scenario('Spectator restarts previously finished game', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user3);
+    });
+    session(username.user2, () => {
+        game.voteAndCheckThatVoteIsVisible(ChessPiece.pawn, ChessTile.a1, ChessPieceValue.pawn);
+    })
+    game.skipMove();
+    session(username.user3, () => {
+        I.waitForText('Game complete');
+        I.click(game.locator.buttons.restartGame);
+        game.voteIsNotVisible(ChessPiece.pawn, ChessTile.a1, ChessPieceValue.pawn);
+    });
+    game.voteAndCheckThatVoteIsVisible(ChessPiece.knight, ChessTile.e2, ChessPieceValue.knight);
+    session(username.user2, () => {
+        game.voteAndCheckThatVoteIsVisible(ChessPiece.king, ChessTile.d3, ChessPieceValue.king);
+        I.waitForText('Game complete');
+        game.voteIsVisible(ChessPiece.king, ChessTile.d3, ChessPieceValue.king);
+        game.voteIsVisible(ChessPiece.knight, ChessTile.e2, ChessPieceValue.knight);
+        game.voteIsNotVisible(ChessPiece.pawn, ChessTile.a1, ChessPieceValue.pawn);
+    })
+});
+
+Scenario('Spectators are displayed at the bottom of players list', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user3);
+    });
+    session(username.user4, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user4);
+        I.seeTextEquals(`${username.user1}`, game.locator.playersList.playerUsernameByIndex(1));
+        I.seeTextEquals(`${username.user3}`, game.locator.playersList.playerUsernameByIndex(2));
+        I.seeTextEquals(`${username.user4} (you)`, game.locator.playersList.playerUsernameByIndex(3));
+        I.seeTextEquals(`${username.user2}`, game.locator.playersList.playerUsernameByIndex(4));
+    });
+    game.vote(ChessPiece.king, ChessTile.c3);
+    session(username.user3, () => {
+        game.vote(ChessPiece.king, ChessTile.c3);
+        I.waitForText('Game complete');
+        I.seeTextEquals(`${username.user2}`, game.locator.playersList.playerUsernameByIndex(3));
+        I.seeTextEquals(`${username.user4}`, game.locator.playersList.playerUsernameByIndex(4));
+    });
+});
+
+Scenario('One of the players chooses spectator role', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+    });
+    session(username.user3, () => {
+        login.spectatorLoginIntoCreatedGameRoom(url, username.user3);
+        game.spectatorGameInProgressView(username.user3);
+        I.refreshPage();
+        game.spectatorGameInProgressView(username.user3);
+    });
+});
