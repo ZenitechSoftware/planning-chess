d96a8b7eefe0948824f83f3f0012a6629b4f9ae5
KristinaKrev <kristina.krevneviciute@zenitech.co.uk>
2023-02-24 13:58:56 +0200

    PLAN-117 created restart game test

---
+++ E2E/tests/restartGame.test.ts
@@ -0,0 +1,22 @@
+Feature('restart game button');
+
+import username = require("../test_data/usernames");
+import ChessTile = require("../test_data/chessTiles");
+import { ChessPiece } from "../test_data/chessPieces";
+import { ChessPieceValue } from "../test_data/chessPieces";
+
+const { I, login, game } = inject();
+
+Scenario('Board is cleared after clicking restart game button', async () => {
+    login.firstVoterLogin(username.user1);
+    let url = await I.grabCurrentUrl();
+    session(username.user2, () => {
+        login.voterLoginIntoCreatedGameRoom(url, username.user2);
+        game.voteAndCheckThatVoteIsVisible(ChessPiece.bishop, ChessTile.a1, ChessPieceValue.bishop);
+    });
+    game.voteAndCheckThatVoteIsVisible(ChessPiece.bishop, ChessTile.c4, ChessPieceValue.bishop);
+    I.waitForText('Game complete');
+    I.click(game.locator.buttons.restartGame);
+    game.voteIsNotVisible(ChessPiece.bishop, ChessTile.c4, ChessPieceValue.bishop);
+    game.voteIsNotVisible(ChessPiece.bishop, ChessTile.a1, ChessPieceValue.bishop);
+  });
