d227a2fbe86a659992c8778a5a1df880faece6d4
Andrius Iljinych <andrius.iljinych@zenitech.co.uk>
2023-03-09 11:35:32 +0200

    PLAN-177 styled a tag, made newGameUrl with useMemo

--- services/app/src/components/header/CreateNewRoomBtn.jsx
+++ services/app/src/components/header/CreateNewRoomBtn.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import Button from '../button/Button';
 import RefreshIcon from '../../static/svg/RefreshIcon.svg';
 import { useUserContext } from '../../contexts/UserContext';
@@ -7,11 +7,11 @@ import { buildPathFromTemplate, ROUTES } from '../../pages/routes';
 const CreateNewRoomBtn = () => {
   const userContext = useUserContext();
 
-  const getNewGameUrl = () => {
+  const newGameUrl = useMemo(() => {
     const newGameId = userContext.generateGameId();
     const gamePath = buildPathFromTemplate(ROUTES.game, {id: newGameId});
     return `${window.location.origin}${gamePath}`
-  }
+  }, []);
 
   return (
     <Button
@@ -19,11 +19,11 @@ const CreateNewRoomBtn = () => {
       dataTestid='header-create-new-room-btn'
     >
       <a
-        href={getNewGameUrl()}
-        className='f-center gap-xs'
+        href={newGameUrl}
+        className='f-center gap-xs decorate-none'
       >
         <img src={RefreshIcon} alt='create new room icon' className='chessboard-icon' />
-        Create new room
+        New room
       </a>
     </Button>
   );
