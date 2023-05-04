b089182d60b689c3c2b8b3122f3b25dc55ec253d
Justas Bartka <>
2023-05-03 23:51:47 -0400

    added end game avatars

--- services/app/src/components/gameFooter/ChessBoardPiece.jsx
+++ services/app/src/components/gameFooter/ChessBoardPiece.jsx
@@ -5,11 +5,18 @@ import './chess-board-pieces.css';
 import { useChessBoardContext } from '../../contexts/ChessBoardContext';
 import PlanningChessTooltip from '../planningChessTooltip/PlanningChessTooltip';
 import { PieceName } from '../../constants/board';
-
+import { GameState } from '../../constants/gameConstants';
+import { Avatar, Tooltip } from 'antd';
+import UserAvatar from '../avatar/UserAvatar';
 const ChessBoardPiece = ({ selectFigure, figureName, figureImg, figureStrength, disabled }) => {
-  const { selectedItem } = useChessBoardContext();
+  const { selectedItem, turns, gameState } = useChessBoardContext();
 
   const skipTooltipTxt = "Mark my move as complete, without any story points";
+
+  console.log(turns)
+  const avatarSize = turns.length > 1
+    ? 'xs'
+    : 's';
 
   const onSelect = () => {
     if (!disabled) {
@@ -17,32 +24,71 @@ const ChessBoardPiece = ({ selectFigure, figureName, figureImg, figureStrength, 
     }
   }
 
+  const tooltipContent = `
+  <div>
+    <div>${turns.filter((element) => element.figure === figureName).length} voted for ${figureStrength}</div>
+    <div>${turns.filter((element) => element.figure === figureName).map((item) => item.player).join(", ")}</div>
+  </div>
+`;
+
   return (
-    <PlanningChessTooltip 
+    <PlanningChessTooltip
       title={figureName === PieceName.SKIP ? skipTooltipTxt : null}
       placement="top"
     >
       <button
         type="button"
-        data-testid={`${figureName}-piece-btn`} 
+        data-testid={`${figureName}-piece-btn`}
         onClick={() => onSelect()}
-        className={classnames('piece-field padding-y-s padding-x-sm f-center rubik-font', {
-          'piece-field-selected': selectedItem === figureName,
-          // disabling with class, because antd appends unnecessary spam around the button when its disabled
-          'disabled': disabled,
-        })}
+        className={classnames(
+          'piece-field padding-y-s padding-x-sm f-center rubik-font',
+          {
+            'piece-field-selected': selectedItem === figureName,
+            // disabling with class, because antd appends unnecessary spam around the button when its disabled
+            disabled: disabled,
+          },
+        )}
       >
-        <img 
-          src={figureImg} 
-          alt={figureName} 
-          className='figure-img'
-        />
-        <p className="figure-title font-size-s weight-500">
-          {figureName}
-        </p>
+        <img src={figureImg} alt={figureName} className="figure-img" />
+        <p className="figure-title font-size-s weight-500">{figureName}</p>
         <div className="figure-strength-container border-r-20 padding-y-0 padding-x-xxs f-center">
-          <p className="figure-strength font-size-xxs weight-500">{figureStrength}</p>
+          <p className="figure-strength font-size-xxs weight-500">
+            {figureStrength}
+          </p>
         </div>
+        <Tooltip
+          title={<div dangerouslySetInnerHTML={{ __html: tooltipContent }} />}
+          placement="top"
+        >
+          <div className="figure-info">
+            {gameState === GameState.GAME_FINISHED && (
+              <Avatar.Group
+                maxCount={2}
+                maxStyle={{
+                  color: 'var(--primary)',
+                  border: '1px solid var(--primary)',
+                  backgroundColor: 'var(--background)',
+                  fontFamily: 'Poppins',
+                }}
+                overlayClassName="hide-group-popover"
+                size={24}
+              >
+                {turns
+                  .filter((element) => element.figure === figureName)
+                  .map((item, index) => (
+                    <UserAvatar
+                      size={avatarSize}
+                      playerId={item.playerId}
+                      key={`bubble-${index}`}
+                      playerInitials={item.player[0]}
+                      dataTestId={`square-${item.playerId}`}
+                      bordered
+                    />
+                  ))}
+              </Avatar.Group>
+            )}
+          </div>
+        </Tooltip>
       </button>
     </PlanningChessTooltip>
   );
